42b250ae75d974ec4d31b904357eec61207d41f1
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-07-23 23:01:40 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I mostly work here all day.
+    I've made a couple of projects  in Python , MERN , js etc. I also like Prompt engineering. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/aaryan2720" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also make videos of public interview  on YouTube. Go sub to <a href="https://www.youtube.com/@ThePublicEye-de2js" target="_blank">my channel</a>! (If you like the content)`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@freecodecamp" target="_blank">Freecodecamp.org</a>, <a href="https://www.youtube.com/@CodeWithHarry" target="_blank">code with harry</a>,
+  <a href="https://www.youtube.com/@cs50" target="_blank">cs50</a> (sometimes)`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://1drv.ms/b/s!AgkG7OJDBbEtn8ApjunlQbyFaM0n_g?e=ubeuAW" target="_blank">Check it out?</a>
+  Contact me at aryan12344choudhari1983@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS.I have done some projects on different  aspects of web development , python and different databases , and more over  I keep intrest in modern technology as LLMs,NLPs etc <a href="https://www.linkedin.com/in/aryan-choudhari-912bab1b1/" target="_blank">here</a>.`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Aaryan's portfolio, just close the tab.`,
+};
